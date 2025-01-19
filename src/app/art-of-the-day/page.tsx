bf9fe0121e5d131d2d482dad517e7153970337ec
Giovana Raphaelli <giovanaraphaelli@outlook.com>
2025-01-19 03:44:32 -0300

    feat: create art of the day page

--- src/app/art-of-the-day/page.tsx
+++ src/app/art-of-the-day/page.tsx
@@ -1,14 +1,61 @@
-export default function ArtOfTheDay() {
+import { Button } from '@/components/ui/button';
+import Link from 'next/link';
+
+const API_BASE_URL = 'https://collectionapi.metmuseum.org/public/collection/v1';
+
+let cachedObjectIDs: number[] | null = null;
+let cacheTimestamp: number | null = null;
+const CACHE_DURATION = 60 * 60 * 1000;
+
+async function fetchObjectIDs() {
+  const now = Date.now();
+
+  if (
+    cachedObjectIDs &&
+    cacheTimestamp &&
+    now - cacheTimestamp < CACHE_DURATION
+  ) {
+    return cachedObjectIDs;
+  }
+
+  const response = await fetch(`${API_BASE_URL}/objects`);
+  const data = await response.json();
+
+  cachedObjectIDs = Array.isArray(data.objectIDs) ? data.objectIDs : [];
+  cacheTimestamp = now;
+
+  return cachedObjectIDs;
+}
+
+export default async function ArtOfTheDay() {
+  const objectIDs = await fetchObjectIDs();
+
+  if (!objectIDs) {
+    return (
+      <div className="h-full flex flex-col items-center justify-center gap-3">
+        <h1 className="text-4xl lg:text-6xl font-serif font-bold">
+          Art of the day
+        </h1>
+        <p className="font-sans text-xs lg:text-sm text-gray-500">
+          No art available at the moment. Please try again later.
+        </p>
+      </div>
+    );
+  }
+
+  const randomIndex = Math.floor(Math.random() * objectIDs.length);
+
   return (
-    <div className="h-full flex flex-col items-center gap-4">
-      <h1 className="text-4xl font-bold">Art of the day</h1>
-      <p className="font-sans text-sm text-center max-w-lg">
-        The Metropolitan Museum of Art presents over 5,000 years of art from
-        around the world for everyone to experience and enjoy. The Museum lives
-        in two iconic sites in New York City—The Met Fifth Avenue and The Met
-        Cloisters. Millions of people also take part in The Met experience
-        online.
-      </p>
+    <div className="h-full flex flex-col items-center justify-center gap-1">
+      <h1 className="text-4xl lg:text-6xl font-serif font-bold">
+        Art of the day
+      </h1>
+      <p className="font-sans text-xs lg:text-sm ">Discover new art today.</p>
+      <Button className="min-w-24 mt-4">
+        <Link href={`/art-of-the-day/${objectIDs[randomIndex]}`}>
+          {`Let's find`}
+        </Link>
+      </Button>
     </div>
   );
 }
