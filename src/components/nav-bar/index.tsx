7ef5e7e65e3b1e804099314264c1f17d89921b09
Giovana Raphaelli <giovanaraphaelli@outlook.com>
2025-01-23 08:47:16 -0300

    refactor: nav-bar

--- src/components/nav-bar/index.tsx
+++ src/components/nav-bar/index.tsx
@@ -2,29 +2,32 @@
 
 import React from 'react';
 import { Button } from '@/components/ui/button';
-import { AlignJustify, X } from 'lucide-react';
+import { AlignJustify, Search } from 'lucide-react';
 import Link from 'next/link';
+import { Drawer, DrawerClose, DrawerContent } from '@/components/ui/drawer';
+import { DialogTitle } from '@radix-ui/react-dialog';
 
 const links = [
   { title: 'Home', href: '/' },
   { title: 'Art of the day', href: '/art-of-the-day' },
-  { title: 'Search', href: '/arts' },
 ];
 
 export function Navbar() {
-  const [isMenuOpen, setIsMenuOpen] = React.useState(false);
+  const [isDrawerOpen, setIsDrawerOpen] = React.useState(false);
 
-  const toggleMenu = () => setIsMenuOpen((prev) => !prev);
+  const toggleDrawer = () => setIsDrawerOpen((prev) => !prev);
 
   return (
-    <header className="sticky top-0 z-50 w-full bg-background ">
+    <header className="sticky top-0 z-50 w-full bg-background">
       <nav className="container mx-auto flex items-center justify-between font-sans p-4">
-        <div className="text-lg font-bold font-serif text-primary leading-5">
-          THE
-          <br />
-          MET
-        </div>
-        <ul className="hidden md:flex gap-6 text-sm font-medium text-muted-foreground">
+        <Link href="/">
+          <div className="text-lg font-bold font-serif text-primary leading-5">
+            THE
+            <br />
+            MET
+          </div>
+        </Link>
+        <ul className="hidden md:flex gap-6 items-center text-sm font-medium text-muted-foreground">
           {links.map((link) => (
             <li key={link.title}>
               <Link
@@ -35,32 +38,61 @@ export function Navbar() {
               </Link>
             </li>
           ))}
+          <li>
+            <Link href="/arts">
+              <Button variant="outline" size="sm">
+                <Search />
+                Search
+              </Button>
+            </Link>
+          </li>
         </ul>
         <Button
           variant="ghost"
           className="md:hidden"
-          onClick={toggleMenu}
-          aria-label="Toggle Menu"
+          onClick={toggleDrawer}
+          aria-label="Open Drawer"
         >
-          {isMenuOpen ? <X /> : <AlignJustify />}
+          <AlignJustify />
         </Button>
-        {isMenuOpen && (
-          <div className="absolute top-16 left-0 w-full bg-card md:hidden">
-            <ul className="flex flex-col gap-4 text-sm p-2 font-medium text-muted-foreground">
+      </nav>
+
+      <Drawer open={isDrawerOpen} onOpenChange={setIsDrawerOpen}>
+        <DrawerContent>
+          <DialogTitle>
+            <Link href="/">
+              <div className="text-lg font-bold font-serif text-primary text-center p-2 mt-2">
+                THE MET
+              </div>
+            </Link>
+          </DialogTitle>
+          <DrawerClose />
+          <div className="p-4">
+            <ul className="flex flex-col gap-2 text-base font-sans font-medium text-center text-muted-foreground">
               {links.map((link) => (
-                <li key={link.title} className="p-2">
+                <li key={link.title}>
                   <Link
-                    className="block hover:text-primary transition-colors"
+                    className="block bg-card p-2 rounded-md"
                     href={link.href}
+                    onClick={() => setIsDrawerOpen(false)}
                   >
                     {link.title}
                   </Link>
                 </li>
               ))}
+              <li>
+                <Link
+                  className="block bg-card p-2 rounded-md"
+                  href="/arts"
+                  onClick={() => setIsDrawerOpen(false)}
+                >
+                  Search
+                </Link>
+              </li>
             </ul>
           </div>
-        )}
-      </nav>
+        </DrawerContent>
+      </Drawer>
     </header>
   );
 }
